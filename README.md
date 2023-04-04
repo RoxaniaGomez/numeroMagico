112cc503b29d2e3bbe0fe03b1ef69442aa447f69
roxania <groxania@gmail.com>
2023-04-04 11:42:13 -0300

    proyecto  completo no deployado aun

---
+++ README.md
@@ -0,0 +1,20 @@
+# Trabajo practico 5 de DOM y BOM en Javascript para RollingCode por Roxania Gomez
+## ejercicio de El numero magico
+_ En este juego la computadora seleccionará aleatoriamente un número entero entre 0 y 50. Tu seguirás adivinando números hasta encontrar el número de la computadora, y la computadora te dirá cada vez si tu intento es demasiado alto o demasiado bajo_
+ ![numero magico](./img/adivino.jpg)
+_Puede ver el proyecto a traves de netlify_
+### https://poo-javascript-rollingcode.netlify.app/
+
+#### Construido con 🛠️
+* [HTML5](https://www.w3schools.com/html/default.asp) - Maquetado estatico
+* [css3](https://www.w3schools.com/css/default.asp) - Estilo
+* [JavaScript](https://www.w3schools.com/js/default.asp) - Dinamismo
+* [Boostrap](https://www.w3schools.com/bootstrap5/index.php) - Estilo
+* [animaciones](https://animate.style) - dinamismo
+
+
+##### Autora✒️
+* **Roxania De Jesus Gomez** - [github](https://github.com/RoxaniaGomez)
+
+###### MUchas gracias por evaluar mi trabajo 🎁
+⌨️ con ❤️ por [Roxania](https://github.com/RoxaniaGomez) 😊
---
+++ js/numeroMagico.js
@@ -0,0 +1,66 @@
+
+let numeroMagico = {
+    // propiedades -> clave: valor;
+    numeroMagicoo: 0,
+    intentos: 0,
+
+    //metodos
+
+    iniciar: function () {
+        this.numeroMagicoo = Math.floor(Math.random() * 2);
+        this.intentos = 0;
+
+        window.alert("El numero magico es un numero entre 1 y 50, luego de elegirlo te dare una pista para encontrarlo, si lo adivinas, Ganas el juego!!!");
+        document.getElementById("iniciar").style.display = "none";
+
+        document.getElementById("formAdivinar").style.display = "initial";
+
+    },
+    adivinar: function (num) {
+        this.intentos = this.intentos + 1;
+        if (num > this.numeroMagicoo) {
+            window.alert("el numero magico es mas chico que el numero que ingresaste");
+        } else if (num < this.numeroMagicoo) {
+            window.alert("el numero magico es mas grande que el numero que ingresaste");
+        } else {
+            if (this.intentos == 1) {
+                document.getElementById("volverAIniciar").style.display = "initial";
+                document.getElementById("formAdivinar").style.display = "none";
+                document.getElementById("cajaIniciar").style.position = 'absolute';
+                document.getElementById("mensajito").style.display = "initial";
+                document.getElementById("mensajito").innerHTML = ("Eres realmente un adivino lo lograste en " + this.intentos + " intento");
+                document.getElementById("adivino").style.display = "initial";
+            } else {
+                document.getElementById("volverAIniciar").style.display = "initial";
+                document.getElementById("formAdivinar").style.display = "none";
+                document.getElementById("cajaIniciar").style.position = 'absolute';
+                document.getElementById("mensajito").style.display = "initial";
+                document.getElementById("mensajito").innerHTML = ("ganaste!! felicidades lo lograste en " + this.intentos + " intentos");
+
+            }
+
+
+
+
+        }
+    },
+    volverAIniciar: function () {
+        document.getElementById("mensajito").style.display = "none";
+        document.getElementById("volverAIniciar").style.display = "none";
+        document.getElementById("adivino").style.display = "none";
+        document.getElementById("cajaIniciar").style.position = 'static';
+        document.getElementById("iniciar").style.display = "initial";
+
+    }
+
+}
+const form2 = document.querySelector('#formAdivinar');
+const numElegido = document.querySelector('#numero');
+
+
+btnAdivinar.addEventListener('click', () => {
+    const numElegido = parseInt(numero.value);
+    numeroMagico.adivinar(numElegido);
+});
+
+
